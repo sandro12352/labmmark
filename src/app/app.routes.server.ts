173fdc47d8aa24d10adcd7c015b4a54a146ded48
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2026-05-02 16:29:35 -0500

    cambio

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,7 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  
+  {
+    path: 'nosotros',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'portafolio/:slug',
+    renderMode: RenderMode.Prerender
+
+  },
   {
     path: '',
     renderMode: RenderMode.Prerender
