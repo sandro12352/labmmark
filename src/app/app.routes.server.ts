b48ac0932a84adb3cc567d0c74b85c0a92981e70
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2026-05-03 03:19:24 -0500

    cambio

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,5 +1,14 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+const PORTFOLIO_SLUGS = [
+  'martial-core',
+  'techpulse',
+  'alma-cafe',
+  'nova-fitness',
+  'urban-living',
+  'meridian-bank',
+];
+
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'nosotros',
@@ -7,8 +16,8 @@ export const serverRoutes: ServerRoute[] = [
   },
   {
     path: 'portafolio/:slug',
-    renderMode: RenderMode.Prerender
-
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => PORTFOLIO_SLUGS.map(slug => ({ slug })),
   },
   {
     path: '',
